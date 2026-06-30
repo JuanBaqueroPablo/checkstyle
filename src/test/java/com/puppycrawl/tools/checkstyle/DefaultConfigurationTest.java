95396676a04a90f1aa3032a21f68d92e85fd4a5c
juanpibaquero <166631434+JuanBaqueroPablo@users.noreply.github.com>
2026-06-30 08:48:33 -0300

    Remove duplicate import statement in DefaultConfigurationTest

--- src/test/java/com/puppycrawl/tools/checkstyle/DefaultConfigurationTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/DefaultConfigurationTest.java
@@ -17,14 +17,13 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-package com.puppycrawl.tools.checkstyle;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.junit.jupiter.api.Test;
 
-import static com.google.common.truth.Truth.assertWithMessage;
 import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
 
 public class DefaultConfigurationTest {
